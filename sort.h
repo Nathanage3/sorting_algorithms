fe0d73e04938aab911eef10582dbbb0d42ea5ef4
nathanage3 <sisay954@gmail.com>
2023-10-24 13:36:42 -0700

    sort.h

--- sort.h
+++ sort.h
@@ -26,5 +26,5 @@ void heap_sort(int *array, size_t size);
 void radix_sort(int *array, size_t size);
 void bitonic_sort(int *array, size_t size);
 void quick_sort_hoare(int *array, size_t size);
-
+void swap_int(int *a, int *b);
 #endif
